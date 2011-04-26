7337afc75320436a506b9db6d5c539040c039dbf
Johan Sveholm <jsveholm@gmail.com>
2011-04-27 00:05:43 +0900

    clickable image

--- lib/generators/nifty/authentication/templates/tests/rspec/layout_steps.rb
+++ lib/generators/nifty/authentication/templates/tests/rspec/layout_steps.rb
@@ -1,7 +1,28 @@
 Then /^I should see "([^"]*)" as (\w+) flash message$/ do |txt,cat|
-  with_scope("div#flash_#{cat}"){ page.text.should eq txt }
+  Then %(I should see "#{txt}" within "div#flash_#{cat}")
 end
 
 Then /^I should see "([^"]*)" as title$/ do |txt|
   Then %(I should see "#{txt}" within "h1")
+end
+
+Then /^I should see "([^"]*)" within the (.+) section$/ do |txt,div|
+  Then %(I should see "#{txt}" within "div##{underscore div}")
+end
+Then /^I should not see "([^"]*)" within the (.+) section$/ do |txt,div|
+  Then %(I should not see "#{txt}" within "div##{underscore div}")
+end
+
+# LINKS -----------------------
+
+When /^I follow "([^"]*)" at the bottom of the page$/ do |lnk|
+  When %(I follow "#{lnk}" within the bottom links section)
+end
+
+When /^I follow "([^"]*)" within the (.+) section$/ do |lnk,div|
+  When %(I follow "#{lnk}" within "div##{underscore(div)}")
+end
+
+When /^I click the image "([^"]*)"$/ do |file|
+  find(:xpath, "//a/img[@alt='#{file}']/..").click
 end
