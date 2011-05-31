d9adc9aa8c91f55486e262dc21ebe5a800699826
Johan Sveholm <jsveholm@gmail.com>
2011-05-31 16:05:28 +0900

    not see no section/form

--- lib/generators/nifty/authentication/templates/tests/rspec/layout_steps.rb
+++ lib/generators/nifty/authentication/templates/tests/rspec/layout_steps.rb
@@ -68,9 +68,12 @@ Then /^I should see (?:a|an) "([^"]*)" (section|form)$/ do |id,cat|
     page.should have_css("form##{id}")
   end 
 end
-
-Then /^I should see no "([^"]*)" section$/ do |id|
-  page.should have_no_css("div##{id}")
+Then /^I should see no "([^"]*)" (section|form)$/ do |id,cat|
+  if cat=="section"
+    page.should have_no_css("div##{id}")
+  elsif cat=="form"
+    page.should have_no_css("form##{id}")
+  end 
 end
 
 Then /^I should see no (\w+) flash message$/ do |cat|
