4c6fd6d9553c10b11bf1a13f67d10e3a16421086
Johan Sveholm <jsveholm@gmail.com>
2011-06-07 10:07:42 +0900

    field existans + button

--- lib/generators/nifty/authentication/templates/tests/rspec/form_steps.rb
+++ lib/generators/nifty/authentication/templates/tests/rspec/form_steps.rb
@@ -47,7 +47,7 @@ end
 When /^I press the button$/ do
   find(:xpath, "//input[@type='submit']").click
 end
-Then /^I should see a "([^"]*)" button$/ do |lbl|
+Then /^I should see (?:a|an) "([^"]*)" button$/ do |lbl|
   page.should have_button(lbl)
 end
 
@@ -58,7 +58,8 @@ Then /^the "([^"]*)" field should be empty$/ do |lbl|
   if field.tag_name == 'textarea'
     field.text.should == ""
   else
-    field.value.should == nil
+    field_value = field.value || ""
+    field_value.should be_empty 
   end
 end
 Then /^the (\w+) "([^"]*)" field should be empty$/ do |ordr,lbl|
@@ -92,6 +93,14 @@ When /^I create (?:a|an) (\w+) with ("[^"]*")((?:, "[^"]*")*)$/ do |mdl, arg1, a
   And %(I press "Create #{mdl.capitalize}")
 end
 
+Then /^I should see fields from "([^"]*)" to "([^"]*)"$/ do |lbl1, lbl2|
+  (lbl1.split[-1].to_i..lbl2.split[-1].to_i).each do |no|
+    Then %(I should see a "#{lbl1.split[0]} #{no}" field)
+  end
+end
+Then /^I should see (?:a|an) "([^"]*)" field$/ do |lbl|
+  page.should have_css("label", :text => lbl)
+end
 Then /^I should see no "([^"]*)" field$/ do |lbl|
   page.should have_no_css("label", :text => lbl)
 end
