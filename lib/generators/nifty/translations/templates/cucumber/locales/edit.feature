39ec84636f4b6be783c68ff95063a37f4433e0b4
Johan Sveholm <jsveholm@gmail.com>
2011-06-14 18:40:25 +0900

    rewritten with cucumber and rspec

--- lib/generators/nifty/translations/templates/cucumber/locales/edit.feature
+++ lib/generators/nifty/translations/templates/cucumber/locales/edit.feature
@@ -7,5 +7,9 @@ Given a pair exists with locale: "en", key: "welcome", value: "Welcome!"
 And a locale exists with title: "en"
 When I go to the translations page
 And I follow "Edit Locale" for key "en.welcome"
-Then the "Title" field should contain "en"
-
+And I fill in "Title" with "en.label"
+And I press "Update Locale"
+Then a locale should exist with title: "en.label"
+And 1 locales should exist
+And I should see "Successfully updated locale." as notice flash message
+And I should be on the translations page
--- lib/generators/nifty/translations/translations_generator.rb
+++ lib/generators/nifty/translations/translations_generator.rb
@@ -33,9 +33,12 @@ module Nifty
         copy_file 'cucumber/translations/index.feature', 'features/translations/index.feature'
         copy_file 'cucumber/translations/new.feature', 'features/translations/new.feature'
         copy_file 'cucumber/locales/new.feature', 'features/locales/new.feature'
+        copy_file 'cucumber/locales/error.feature', 'features/locales/error.feature'
         copy_file 'cucumber/locales/edit.feature', 'features/locales/edit.feature'
         copy_file 'cucumber/locales/view.feature', 'features/locales/view.feature'
         copy_file 'cucumber/redis_steps.rb', 'features/step_definitions/redis_steps.rb'
+        copy_file 'rspec/translations_controller_spec.rb', 'spec/controllers/translations_controller_spec.rb'
+        copy_file 'rspec/locales_controller_spec.rb', 'spec/controllers/locales_controller_spec.rb'
         copy_file '20110610051822_create_locales.rb', 'db/migrate/20110610051822_create_locales.rb'
         copy_file '20110610060750_create_translations.rb', 'db/migrate/20110610060750_create_translations.rb'
         route "    delete 'delete'"
