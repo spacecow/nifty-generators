7e2be36d2361c1461f202283ac238a4fc21323e7
Johan Sveholm <jsveholm@gmail.com>
2011-07-01 15:11:57 +0900

    error message

--- lib/generators/nifty/resets/resets_generator.rb
+++ lib/generators/nifty/resets/resets_generator.rb
@@ -11,8 +11,20 @@ module Nifty
         if options.haml?
         else
         end
+        copy_file 'cucumber/resets/new.feature', 'features/resets/new.feature'
         copy_file 'resets_controller.rb', 'app/controllers/resets_controller.rb'
+        copy_file '20110627045914_create_resets.rb', 'db/migrate/20110627045914_create_resets.rb'
+        copy_file 'reset.rb', 'app/models/reset.rb'
+        copy_file 'new.html.erb', 'app/views/resets/new.html.erb'
+        copy_file 'reset_confirmation.text.erb', 'app/views/reset_mailer/reset_confirmation.text.erb'
+        copy_file 'reset_mailer.rb', 'app/mailers/reset_mailer.rb'
+        copy_file 'reset_password_controller.rb', 'app/models/reset_password_controller.rb'
+        copy_file 'change_password.html.erb', 'app/views/users/change_password.html.erb'
+        
         route "resources :resets, :only => [:new,:create]"
+        route "# FOR USERS"
+        route "    get 'change_password'"
+        route "    put 'update_password'"
       end
 
       private
